58936e383eac8a831be04c2672e834fed3f1aa18
dencoded <33698537+dencoded@users.noreply.github.com>
2018-05-24 23:47:22 -0400

    config load fixed, regexp cache default expiration > 0, key and value size guards added

--- main.go
+++ main.go
@@ -894,7 +894,7 @@ func afterConfSetup(conf *config.Config) {
 	GlobalRPCPingTimeout = time.Second * time.Duration(conf.SlaveOptions.PingTimeout)
 	GlobalRPCCallTimeout = time.Second * time.Duration(conf.SlaveOptions.CallTimeout)
 	initGenericEventHandlers(conf)
-	tykregexp.ResetCache(time.Second*time.Duration(conf.RegexpCacheExpire), conf.DisableRegexpCache)
+	tykregexp.ResetCache(time.Second*time.Duration(conf.RegexpCacheExpire), !conf.DisableRegexpCache)
 }
 
 var hostDetails struct {
